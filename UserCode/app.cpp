496a8e93d0679c244cdbc89a032e198ec2b69cab
syhanjin <syhanjin666@gmail.com>
2026-03-17 20:16:29 +0800

    test: 无上位机矫正移动测试
    临时将定位初始化提前，否则会报错

--- UserCode/app.cpp
+++ UserCode/app.cpp
@@ -27,6 +27,12 @@ void TIM_Callback_100Hz(TIM_HandleTypeDef* htim)
     Chassis_Update_100Hz();
 }
 
+chassis::Posture  pos;
+chassis::Velocity vel;
+
+// 0
+uint8_t s = 0;
+
 /**
  * @brief Function implementing the initTask thread.
  * @param argument: Not used
@@ -42,25 +48,52 @@ extern "C" void Init(void* argument)
 
     Protocol_Init();
 
+    ChassisLoc_Init();
+
     // 启动定时器
     HAL_TIM_RegisterCallback(&htim5, HAL_TIM_PERIOD_ELAPSED_CB_ID, TIM_Callback_1kHz_1);
     HAL_TIM_RegisterCallback(&htim5, HAL_TIM_OC_DELAY_ELAPSED_CB_ID, TIM_Callback_1kHz_2);
     HAL_TIM_Base_Start_IT(&htim5);
     HAL_TIM_OC_Start_IT(&htim5, TIM_CHANNEL_1);
 
-    while (!init_pos_received)
-        osDelay(1);
-
-    ChassisLoc_Init();
+    // while (!init_pos_received)
+    //     osDelay(1);
 
     Device_WaitAllConnected();
 
     osDelay(2000);
 
-    for (;;)
-        osDelay(1);
-
     Chassis_Init();
+
+    using namespace chassis;
+    Velocity v = vel;
+    Posture  p = pos;
+
+    for (;;)
+    {
+        if (s == 0)
+        {
+            chassis_->stop();
+        }
+        else if (s == 1)
+        {
+            if (vel.vx != v.vx || vel.vy != v.vy || vel.wz != v.wz)
+            {
+                v = vel;
+                chassis_->setVelocityInBody(v, false);
+            }
+        }
+        else if (s == 2)
+        {
+            if (pos.x != p.x || pos.y != p.y || pos.yaw != p.yaw)
+            {
+                p = pos;
+                chassis_->setTargetPostureInWorld(p);
+            }
+        }
+
+        osDelay(1);
+    }
 
     /* 初始化完成后退出线程 */
     osThreadExit();
