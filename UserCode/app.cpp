2e8c364e453ff901ee2b3da6d5e4e36231f745e7
syhanjin <syhanjin666@gmail.com>
2026-03-23 14:32:31 +0800

    feat: 移除全部测试代码并更新依赖驱动

--- UserCode/app.cpp
+++ UserCode/app.cpp
@@ -4,17 +4,12 @@
 #include "protocol.hpp"
 #include "system.hpp"
 #include "tim.h"
-bool init_pos_received = false;
-
-uint32_t pc_time;
 
 void TIM_Callback_1kHz_1(TIM_HandleTypeDef* htim)
 {
     Device::update_1kHz();
 
     service::Watchdog::EatAll();
-
-    pc_time = Protocol::clock_->getPCTime();
 }
 
 void TIM_Callback_1kHz_2(TIM_HandleTypeDef* htim)
@@ -29,18 +24,12 @@ void TIM_Callback_100Hz(TIM_HandleTypeDef* htim)
     Chassis::update_100Hz();
 }
 
-chassis::Posture  pos;
-chassis::Velocity vel;
-
-// 0
-uint8_t s = 0;
-
 /**
  * @brief Function implementing the initTask thread.
  * @param argument: Not used
  * @retval None
  */
-extern "C" void Init(void* argument)
+extern "C" [[noreturn]] void Init(void* argument)
 {
     /* 初始化代码 */
 
@@ -63,66 +52,10 @@ extern "C" void Init(void* argument)
     osDelay(2000);
 
     // waiting for system inited
-    // while (!System::Init::inited())
-    //     osDelay(1);
-
-    constexpr chassis::Posture init = { 0, 0, 0 };
-    Chassis::locCtrlInit(init);
+    while (!System::Init::inited())
+        osDelay(1);
 
     Chassis::ctrl->enable();
-
-    chassis::Velocity v = vel;
-    chassis::Posture  p = pos;
-
-    osDelay(5000);
-
-    for (;;)
-    {
-        // Chassis::ctrl->setTargetPostureInWorld({0, 0, 0});
-        // Chassis::ctrl->waitTrajectoryFinish();
-        // osDelay(500);
-        Chassis::ctrl->setTargetPostureInWorld({ 3, 0, 0 });
-        Chassis::ctrl->waitTrajectoryFinish();
-        osDelay(500);
-        Chassis::ctrl->setTargetPostureInWorld({ 3, 0, 90 });
-        Chassis::ctrl->waitTrajectoryFinish();
-        osDelay(500);
-        Chassis::ctrl->setTargetPostureInWorld({ 3, -3, 135 });
-        Chassis::ctrl->waitTrajectoryFinish();
-        osDelay(500);
-        Chassis::ctrl->setTargetPostureInWorld({ 0, 0, 135 });
-        Chassis::ctrl->waitTrajectoryFinish();
-        osDelay(500);
-        Chassis::ctrl->setTargetPostureInWorld({ 0, 0, 0 });
-        Chassis::ctrl->waitTrajectoryFinish();
-        osDelay(10000);
-    }
-
-    // for (;;)
-    // {
-    //     if (s == 0)
-    //     {
-    //         Chassis::ctrl->stop();
-    //     }
-    //     else if (s == 1)
-    //     {
-    //         if (vel.vx != v.vx || vel.vy != v.vy || vel.wz != v.wz)
-    //         {
-    //             v = vel;
-    //             Chassis::ctrl->setVelocityInBody(v, false);
-    //         }
-    //     }
-    //     else if (s == 2)
-    //     {
-    //         if (pos.x != p.x || pos.y != p.y || pos.yaw != p.yaw)
-    //         {
-    //             p = pos;
-    //             Chassis::ctrl->setTargetPostureInWorld(p);
-    //         }
-    //     }
-
-    //     osDelay(1);
-    // }
 
     /* 初始化完成后退出线程 */
     osThreadExit();
